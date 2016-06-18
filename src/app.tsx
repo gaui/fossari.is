e145e8106519637534ceb32c8e248891e8d9b544
Guðjón Jónsson <gudjonj@gmail.com>
2016-06-18 00:36:53 +0000

    Updated app to new TypeScript syntax

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
 import IsItFriday from './components/IsItFriday/IsItFriday';
 import Countdown from './components/Countdown/Countdown';
-require('./app.less');
+
+import './app.less';
 
 // Hide everything until DOM has been loaded
 document.addEventListener('DOMContentLoaded', function(event) {
     document.getElementById('wrapper').style.display = 'flex';
 });
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+interface AppState {
+  friday : boolean
+}
+
+class App extends React.Component<{}, AppState> {
+  private interval : number;
+
+  constructor() {
+      super();
 
     let self = this;
     self.state = this.getNextState();
---
+++ src/components/IsItFriday/IsItFriday.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+
+interface IsItFridayProps {
+  friday : boolean
+}
+
+interface IsItFridayState {
+  text : string,
+  cssClass : string
+}
+
+export default class IsItFriday extends React.Component<IsItFridayProps, IsItFridayState> {
+  constructor(props : IsItFridayProps) {
+    super(props);
+
+    this.state = this.getNextState();
+  }
+
+  componentWillReceiveProps() {
+    this.setState(this.getNextState());
+  }
+
+  getNextState(): IsItFridayState {
+    let map = {
+      yes: {
+        text: 'já',
+        cssClass: 'yes'
+      },
+      no: {
+        text: 'nei',
+        cssClass: 'no'
+      }
+    };
+
+    return this.props.friday ? map['yes'] : map['no'];
+  }
+
+  render() {
+    return (
+      <div id="IsItFriday">
+        <div>Er fössari?</div>
+        <div className={this.state.cssClass}>{this.state.text}</div>
+      </div>
+    )
+  }
+}
