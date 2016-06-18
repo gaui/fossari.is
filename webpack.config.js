c39001c3231465a0e9331dfbe6911d6afe4f8903
Guðjón Jónsson <gudjonj@gmail.com>
2016-06-18 00:35:43 +0000

    Updated Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,33 +4,24 @@ var path = require('path');
 module.exports = {
   context: path.join(__dirname, '/src'),
   entry: {
-    app: './app.js',
-    vendor: ['react']
+    app: './app.tsx',
+    vendor: ['react', 'react-dom', 'moment']
   },
   output: {
     path: path.join(__dirname, '/dist'),
     filename: 'app.js'
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx', '.ts', '.tsx']
   },
   module: {
     loaders: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.less$/,
-        loader: 'style!css!less',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpg|woff|woff2|ttf|eot|svg)(\?]?.*)?$/,
-        loader : 'file',
-        exclude: /node_modules/
-      }
+      { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/ },
+      { test: /\.less$/, loader: 'style!css!less', exclude: /node_modules/ },
+      { test: /\.(png|jpg|woff|woff2|ttf|eot|svg)(\?]?.*)?$/, loader : 'file', exclude: /node_modules/ }
+    ],
+    preLoaders: [
+      { test: /\.js$/, loader: 'source-map-loader', exclude: /node_modules/ }
     ]
   },
   plugins: [
