886a59e49acfbc6b7a0252aaaf3148be6b5fd96a
Guðjón Jónsson <gudjonj@gmail.com>
2017-04-16 18:34:29 +0000

    Upgraded library dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,45 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var GitRevisionPlugin = require('git-revision-webpack-plugin');
+
+var gitRevisionPluginOpt = {
+    versionCommand: 'describe --always --tags',
+    commithashCommand: 'rev-parse --short HEAD'
+};
+var gitRevisionPlugin = new GitRevisionPlugin(gitRevisionPluginOpt);
 
 module.exports = {
-  context: path.join(__dirname, '/src'),
   entry: {
-    app: './app.tsx',
-    vendor: ['react', 'react-dom', 'moment']
+    app: './src/app.tsx',
+    vendors: ['react', 'react-dom', 'moment']
   },
   output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'app.js'
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx', '.ts', '.tsx']
+    filename: '[name].[git-revision-hash].js',
+    path: path.resolve(__dirname, './dist')
   },
   module: {
-    loaders: [
+    rules: [
       { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/ },
-      { test: /\.less$/, loader: 'style!css!less', exclude: /node_modules/ },
-      { test: /\.(png|jpg|woff|woff2|ttf|eot|svg)(\?]?.*)?$/, loader : 'file', exclude: /node_modules/ }
-    ],
-    preLoaders: [
-      { test: /\.js$/, loader: 'source-map-loader', exclude: /node_modules/ }
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader', exclude: /node_modules/ },
+      { test: /\.(png|jpg|woff|woff2|ttf|eot|svg)(\?]?.*)?$/, loader : 'file-loader', exclude: /node_modules/ }
     ]
   },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
-    })
+    new webpack.EnvironmentPlugin(['NODE_ENV']),
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendors' }),
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en-gb|is/),
+    gitRevisionPlugin,
+    new HtmlWebpackPlugin({
+      title: 'Fössari.is',
+      template: path.resolve(__dirname, './src/index.ejs'),
+      gitCommitHash: gitRevisionPlugin.commithash(),
+      gitCommitVersion: gitRevisionPlugin.version()
+    }),
+    new CleanWebpackPlugin(['dist'])
   ]
 };
