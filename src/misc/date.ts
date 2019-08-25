f6b340501a7e5e9ea6eb8eea5d587d14320335db
Gudjon Jonsson <gudjonj@gmail.com>
2019-08-25 23:38:22 +0000

    Improved 'getDifference' function, include more detailed date part breakdown

--- src/misc/date.ts
+++ src/misc/date.ts
@@ -1,8 +1,4 @@
 import {
-  differenceInDays,
-  differenceInHours,
-  differenceInMinutes,
-  differenceInSeconds,
   isAfter,
   isFriday as friday,
   isSameDay as sameDay,
@@ -18,13 +14,31 @@ function getNextWeekDay(date: Date, weekDay: number): Date {
   return next;
 }
 
-function getDifference(dateTo: Date, dateFrom: Date): CountdownTimerProps {
-  return {
-    days: differenceInDays(dateFrom, dateTo).toLocaleString('de-GB'),
-    hours: differenceInHours(dateFrom, dateTo).toLocaleString('de-GB'),
-    minutes: differenceInMinutes(dateFrom, dateTo).toLocaleString('de-GB'),
-    seconds: differenceInSeconds(dateFrom, dateTo).toLocaleString('de-GB')
+function getDifference(targetDate: Date, sourceDate: Date): DateDetail {
+  const r: DateDetail = {} as DateDetail;
+  const s: DateDetail = {
+    years: 31536000,
+    months: 2592000,
+    weeks: 604800,
+    days: 86400,
+    hours: 3600,
+    minutes: 60,
+    seconds: 1,
+    milliseconds: 1
   };
+
+  const calc = (key: string) =>
+    key === 'milliseconds' ? s[key] : s[key] * 1000;
+
+  const diff = targetDate.getTime() - sourceDate.getTime();
+  let d = Math.abs(diff);
+  Object.entries(s).forEach(([key]) => {
+    const datePart = calc(key) || 0;
+    r[key] = Math.floor(d / datePart);
+    d -= r[key] * datePart;
+  });
+
+  return r;
 }
 
 export {
