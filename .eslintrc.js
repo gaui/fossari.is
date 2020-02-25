dd9559ca678d1103c20db038ecfb11e57fcb366d
Gudjon Jonsson <gudjonj@gmail.com>
2020-02-25 04:01:34 +0000

    chore: Turning off ESLint rule 'explicit-function-return-type'

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-use-before-define': ['error', 'nofunc']
   }
 };
